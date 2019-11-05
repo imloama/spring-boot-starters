6c262b86c640d857b3d64cd3add04f82669e10b2
max <imloama@gmail.com>
2019-11-05 21:20:16 +0800

    feat: init

---
+++ spring-boot-starter-admin/spring-boot-starter-admin-api/src/test/java/com/github/imloama/admin/SpringBootStarterAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.imloama.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterAdminApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
